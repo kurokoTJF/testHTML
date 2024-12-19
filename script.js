40d04800234dc1a63b3739630601c305b85b7993
kurokoTJF <117005966+kurokoTJF@users.noreply.github.com>
2024-12-19 14:25:31 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "cyan";
+ctx.fillRect(50, 50, 100, 100);
+
+console.log("ゲームが正常に読み込まれました！");
